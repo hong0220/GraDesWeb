66f036803b7112951fabafd0307988dbd864f29e
hong0220 <hong0220@github.com>
2015-04-07 12:44:33 +0800

    hong0220

---
+++ src/com/mining/weibo/controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.mining.weibo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping(value = "statistic")
+	public String statisticTopicSentiment() {
+		return "core/statistics";
+	}
+
+	@RequestMapping(value = "hello")
+	public void hello() {
+		System.out.println("hello");
+	}
+}
